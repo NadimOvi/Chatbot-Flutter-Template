6289ad48a7ed3227b519148c081a44eeeefae95e
NadimOvi <nadimovi20@gmail.com>
2025-10-17 04:27:33 +0200

    Gemini API Added

---
+++ lib/data/urls.dart
@@ -0,0 +1,5 @@
+class Urls {
+  static const String geminiBaseUrl =
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent";
+  static const String geminiApiKey = "AIzaSyAVhjMwUKGXK4pjsrwoO1OZI-S4PCgm43s"; // Store securely
+}
